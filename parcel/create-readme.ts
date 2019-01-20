e1c659014bc3c02a720ab3dd60eecdaf8ad3beeb
Natan Farkash <natqe3@gmail.com>
2019-01-20 10:22:54 +0200

    test

--- parcel/create-readme.ts
+++ parcel/create-readme.ts
@@ -22,8 +22,6 @@ export const createReadme = (fileName: string, generalReadme: string, packageJSO
     result = result.replace(`### ${nameCamel}`, `# ${nameCamel}`).replace(`utilizes/${fileName}`, `${packageJSONObject.name}`)
 
     result += `\nThis module exported from [utilizes](https://www.npmjs.com/package/utilizes) project.`
-// TODO remove
-    result += `<!-- -->`
 
     return promisify(writeFile)(`sub/${fileName}/readme.md`, result)
 
