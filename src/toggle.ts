67079973082decf8f2f3e4787c0ac20f3eeb4fe2
Natan Farkash <natqe3@gmail.com>
2019-02-03 22:20:15 +0200

    fix type when first argument is array with one

--- src/toggle.ts
+++ src/toggle.ts
@@ -9,22 +9,23 @@ type PropertyPath = PropertyName | ReadonlyArray<PropertyName>
 
 type falsy = false | 0 | '' | null | undefined
 
-export function toggle<TObject extends object, TKey extends keyof TObject, I = true | false>(object: TObject, path: TKey | [TKey], ...values: [[I]]): I | (I extends falsy ? true : false)
-export function toggle<TObject extends object, TKey extends keyof TObject, I = true | false>(object: TObject, path: TKey | [TKey], ...values:[Array<I>]): typeof values[0]['length'] extends 0 ? boolean : I
-export function toggle<T, I = true | false>(object: { [index: number]: T } | { [index: string]: T }, path: number, ...values: [[I]]): I | (I extends falsy ? true : false)
+export function toggle<TObject extends object, TKey extends keyof TObject, I = true | false>(object: TObject, path: TKey | [TKey], ...values: [[I]]): I | boolean
+export function toggle<TObject extends object, TKey extends keyof TObject, I = true | false>(object: TObject, path: TKey | [TKey], ...values: [Array<I>]): typeof values[0]['length'] extends 0 ? boolean : I
+export function toggle<T, I = true | false>(object: { [index: number]: T } | { [index: string]: T }, path: number, ...values: [[I]]): I | boolean
 export function toggle<T, I = true | false>(object: { [index: number]: T } | { [index: string]: T }, path: number, ...values: [Array<I>]): typeof values[0]['length'] extends 0 ? boolean : I
-export function toggle<I = true | false>(object, path: PropertyPath, ...values: [[I]]): I | (I extends falsy ? true : false)
+export function toggle<I = true | false>(object, path: PropertyPath, ...values: [[I]]): I | boolean
 export function toggle<I = true | false>(object, path: PropertyPath, ...values: [Array<I>]): typeof values[0]['length'] extends 0 ? boolean : I
-export function toggle<TObject extends object, TKey extends keyof TObject, I = true | false>(object: TObject, path: TKey | [TKey], ...values: [I]): I | (I extends falsy ? true : false)
+export function toggle<TObject extends object, TKey extends keyof TObject, I = true | false>(object: TObject, path: TKey | [TKey], ...values: [I]): I | (typeof values[0] extends falsy ? true : false)
 export function toggle<TObject extends object, TKey extends keyof TObject, I = true | false>(object: TObject, path: TKey | [TKey], ...values: Array<I>): typeof values['length'] extends 0 ? boolean : I
-export function toggle<T, I = true | false>(object: { [index: number]: T } | { [index: string]: T }, path: number, ...values: [I]): I | (I extends falsy ? true : false)
+export function toggle<T, I = true | false>(object: { [index: number]: T } | { [index: string]: T }, path: number, ...values: [I]): I | (typeof values[0] extends falsy ? true : false)
 export function toggle<T, I = true | false>(object: { [index: number]: T } | { [index: string]: T }, path: number, ...values: Array<I>): typeof values['length'] extends 0 ? boolean : I
-export function toggle<I = true | false>(object, path: PropertyPath, ...values: [I]): I | (I extends falsy ? true : false)
+export function toggle<I = true | false>(object, path: PropertyPath, ...values: [I]): I | (typeof values[0] extends falsy ? true : false)
 export function toggle<I = true | false>(object, path: PropertyPath, ...values: Array<I>): typeof values['length'] extends 0 ? boolean : I
 export function toggle<TObject extends object, TKey extends keyof TObject, T, I = true | false>(
   object: { [index: number]: T } | { [index: string]: T } | TObject | any,
   path: PropertyPath | number | TKey | [TKey],
-  ...values: Array<I> | [I] | [Array<I>]): typeof values['length'] extends 0 ? boolean : I {
+  ...values: Array<I> | [I] | [Array<I>]):
+  typeof values['length'] extends 0 ? boolean : I {
 
   let valuesToWorkWith: Array<boolean | I>
 
