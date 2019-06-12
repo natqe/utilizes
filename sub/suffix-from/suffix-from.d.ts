fec48f2d2f169de5534eaa448ba337ca298a08c5
Natan Farkash <natqe3@gmail.com>
2019-06-12 20:19:21 +0300

    publish

--- sub/suffix-from/suffix-from.d.ts
+++ sub/suffix-from/suffix-from.d.ts
@@ -1,2 +1,8 @@
+/**
+ * @description Get suffix from string
+ * @param {string} subject subject The string to get the suffix from
+ * @param {(string | RegExp)} splitter splitter From where the suffix should be start from
+ * @returns {string} The suffix that found
+ */
 export declare function suffixFrom(subject: string, splitter: RegExp): string;
 export declare function suffixFrom(subject: string, splitter: string): string;
