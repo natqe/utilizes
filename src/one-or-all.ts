299b1cb3b98edd65162973e247a63e0d1b2dca65
Natan Farkash <natqe3@gmail.com>
2019-09-19 14:22:07 +0300

    add callback to one or all

--- src/one-or-all.ts
+++ src/one-or-all.ts
@@ -1,9 +1,17 @@
 import isArray from 'lodash/isArray'
 import size from 'lodash/size'
 
-export const oneOrAll = <T>(array: T | ReadonlyArray<T>): T | ReadonlyArray<T> => {
-  if (!isArray(array) || size(array) > 1) return array
-  else if (size(array) === 1) return array[0]
+export function oneOrAll<T>(item: T): T
+export function oneOrAll<T>(items: Array<T>): Array<T>
+export function oneOrAll<T>(value: []): void
+export function oneOrAll<T>(items: [T]): T
+export function oneOrAll<T, R>(items: T, callbackfn: (value: T) => R): R
+export function oneOrAll<T, R>(items: [T], callbackfn: (value: T) => R): R
+export function oneOrAll<T, R>(items: Array<T>, callbackfn: (value: T, index?: number, array?: Array<T>) => R): R
+export function oneOrAll<R>(array, callbackfn: (value, index?: number, array?: Array<any>) => R): R
+export function oneOrAll(array, callbackfn?) {
+  if (!isArray(array) || size(array) > 1) return callbackfn ? isArray(array) ? array.map(callbackfn) : callbackfn(array) : array
+  else if (size(array) === 1) return callbackfn ? callbackfn(array[0]) : array[0]
 }
 
 export default oneOrAll
