a96dc2e44d62cd7057155b4c9f4c6300d26251d8
Natan Farkash <natqe3@gmail.com>
2019-09-19 14:41:22 +0300

    publish

--- sub/one-or-all/one-or-all.d.ts
+++ sub/one-or-all/one-or-all.d.ts
@@ -1,2 +1,9 @@
-export declare const oneOrAll: <T>(array: T | readonly T[]) => T | readonly T[];
+export declare function oneOrAll<T>(item: T): T;
+export declare function oneOrAll<T>(items: Array<T>): Array<T>;
+export declare function oneOrAll<T>(value: []): void;
+export declare function oneOrAll<T>(items: [T]): T;
+export declare function oneOrAll<T, R>(items: T, callbackfn: (value: T) => R): R;
+export declare function oneOrAll<T, R>(items: [T], callbackfn: (value: T) => R): R;
+export declare function oneOrAll<T, R>(items: Array<T>, callbackfn: (value: T, index?: number, array?: Array<T>) => R): R;
+export declare function oneOrAll<R>(array: any, callbackfn: (value: any, index?: number, array?: Array<any>) => R): R;
 export default oneOrAll;
