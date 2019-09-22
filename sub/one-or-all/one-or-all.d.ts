a7e8ac7fe4dc85238da2ebc87d48b5a597101f53
Natan Farkash <natqe3@gmail.com>
2019-09-22 07:15:02 +0300

    publish

--- sub/one-or-all/one-or-all.d.ts
+++ sub/one-or-all/one-or-all.d.ts
@@ -1,2 +1,3 @@
-export declare function oneOrAll<T, R>(value: T | Array<T>, callbackfn?: (item: T, index?: number, array?: Array<T>) => R): R | Array<R>;
+export declare function oneOrAll<T, R>(value: T | Array<T>, callbackfn: (item: T, index?: number, array?: Array<T>) => R): R | Array<R>;
+export declare function oneOrAll<T>(value: T | Array<T>): T | Array<T>;
 export default oneOrAll;
